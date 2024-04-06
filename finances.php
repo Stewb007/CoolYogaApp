12314987e982dd38f580bd75d55c2f01d90242ba
Arshia <ars.khan50@protonmail.com>
2024-04-06 16:21:08 -0400

    Final update

--- finances.php
+++ finances.php
@@ -1,7 +1,11 @@
 <?php
 
 $error = '';
+$error_expense = '';
 $name = '';
+$balance = '';
+$category = '';
+$payed = '';
 $email = '';
 $subject = '';
 $message = '';
@@ -76,14 +80,73 @@ if(isset($_POST["delete-submit"]))
     }
 }
 
+if (file_exists('expenses.csv') && filesize('expenses.csv') > 0) {
+    $expenses_data = array_map('str_getcsv', file('expenses.csv'));
+} else {
+    $expenses_data = [];
+}
+
+if (isset($_POST["submit-expense"])) {
+    if (empty($_POST["name"]) || empty($_POST["category"]) || empty($_POST["payed"]) || empty($_POST["balance"])) {
+        $error_expense .= '<p><label class="text-danger">All fields are required to add</label></p>';
+    } else {
+        $name = clean_text($_POST["name"]);
+        $category = clean_text($_POST["category"]);
+        $payed = clean_text($_POST["payed"]);
+        $balance = clean_text($_POST["balance"]);
+
+        $expenses_csv = fopen("expenses.csv", "a");
+        $expense_data = array($name, $category, $payed, $balance);
+        fputcsv($expenses_csv, $expense_data);
+        fclose($expenses_csv);
+
+        // Reload CSV data
+        $expenses_data = array_map('str_getcsv', file('expenses.csv'));
+
+        $name = '';
+        $category = '';
+        $payed = '';
+        $balance = '';
+        $error_expense = '<label class="text-success">Expense Added Successfully!</label>';
+    }
+}
+
+if (isset($_POST["delete-expense-submit"])) {
+    if (isset($_POST['delete']) && !empty($_POST['delete'])) {
+        $delete_rows = $_POST['delete'];
+        $new_expenses_data = [];
+
+        foreach($expenses_data as $key => $row) {
+            if (!in_array($key, $delete_rows)) {
+                $new_expenses_data[] = $row;
+            }
+        }
+
+        $file = fopen('expenses.csv', 'w');
+        foreach($new_expenses_data as $fields) {
+            fputcsv($file, $fields);
+        }
+        fclose($file);
+
+        // Reload CSV data
+        $expenses_data = array_map('str_getcsv', file('expenses.csv'));
+
+        header("Location: {$_SERVER['REQUEST_URI']}");
+        exit;
+    } else {
+        $error_expense = '<label class="text-danger">Please select at least one row to delete</label>';
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Cool Yoga app database</title>
+    <script src="https://kit.fontawesome.com/bbdcfd916b.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@700&display=swap" rel="stylesheet">
+    <title>Cool Yoga app database</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -91,6 +154,11 @@ if(isset($_POST["delete-submit"]))
         function showForm() {
             document.getElementById('add-button').style.display = 'none';
             document.getElementById('form-container').style.display = 'block';
+        }
+
+        function showExpenseForm() {
+            document.getElementById('add-expense-button').style.display = 'none';
+            document.getElementById('form-expense-container').style.display = 'block';
         }
 
         function toggleCheckbox(element) {
@@ -116,113 +184,170 @@ if(isset($_POST["delete-submit"]))
             return true;
         }
     </script>
-    <head>
-    <title>Cool Yoga app database</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    <style>
-        /* Importing fontawesome styles */
-        @import url('https://kit-free.fontawesome.com/releases/latest/css/free.min.css');
-        
-        /* Setting font-family for all text */
-        body, button, input, select, textarea {
-            font-family: 'Roboto', sans-serif;
-        }
-        
-        /* Set background color to black and text color to white a*/
-        body {
-            background-color: #141414;
-            color: white;
-        }
-        .add-button {
-            background-color: #007bff; /* Blue color */
-            color: white;
-            border: none;
-            padding: 15px 30px; /* Larger padding for bigger button */
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 20px; /* Larger font size */
-            margin: 10px 2px; /* Increased margin */
-            cursor: pointer;
-            border-radius: 8px;
-            transition: background-color 0.3s ease; /* Smooth transition effect */
-        }
-
-        /* Hover effect for the add button */
-        .add-button:hover {
-            background-color: #004080; /* Darker blue color on hover */
-        }
-    </style>
-    <!-- Font Awesome script -->
-    <script src="https://kit.fontawesome.com/bbdcfd916b.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <br />
+<style>
+    .container h3, h2 {
+        background: linear-gradient(to top, #5593DC 0%, #00326a 100%);
+        -webkit-background-clip: text;
+        color: transparent;
+        font-weight: bold;
+    }
+
+    .col-md-6 th, td {
+        color: #fff;
+    }
+
+    body {
+        background-color: #141414;
+    }
+
+    body, button, input, textarea {
+        font-family: 'Roboto', sans-serif;
+    }
+
+    .table-container {
+        margin-bottom: 20px;
+    }
+</style>
 <div class="container">
     <h2 align="center">Cool Yoga App Database</h2>
     <br />
-    <div class="col-md-6">
-        <h3 align="center">CSV Data</h3>
-        <form method="post" onsubmit="return validateDelete()">
-            <table class="table table-bordered">
-                <thead>
-                <tr>
-                    <th><input type="checkbox" onclick="toggleCheckbox(this)"></th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Subject</th>
-                    <th>Message</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php foreach($csv_data as $key => $row): ?>
-                    <tr>
-                        <td><input type="checkbox" name="delete[]" value="<?php echo $key; ?>"></td>
-                        <td><?php echo $row[0]; ?></td>
-                        <td><?php echo $row[1]; ?></td>
-                        <td><?php echo $row[2]; ?></td>
-                        <td><?php echo $row[3]; ?></td>
+    <div class="row">
+        <div class="col-md-12 table-container">
+            <h3 align="center">CSV Data</h3>
+            <form method="post" onsubmit="return validateDelete()">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr style="font-weight: bold; color: white;">
+                        <th><input type="checkbox" onclick="toggleCheckbox(this)"></th>
+                        <th>Name</th>
+                        <th># of Classes Attended</th>
+                        <th>Subject</th>
+                        <th>Oustanding Balance ($)</th>
                     </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
-            <div align="center">
-                <button type="submit" name="delete-submit" class="btn btn-danger">Delete</button>
-            </div>
-            <?php echo $error; ?>
-        </form>
+                    </thead>
+                    <tbody>
+                    <?php foreach($csv_data as $key => $row): ?>
+                        <tr>
+                            <td><input type="checkbox" name="delete[]" value="<?php echo $key; ?>"></td>
+                            <td><?php echo $row[0]; ?></td>
+                            <td><?php echo $row[1]; ?></td>
+                            <td><?php echo $row[2]; ?></td>
+                            <td><?php echo $row[3]; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+                <div align="center">
+                    <button type="submit" name="delete-submit" class="btn btn-danger">Delete</button>
+                </div>
+                <?php echo $error; ?>
+            </form>
+        </div>
     </div>
-    <div id="add-button" align="center">
-        <button class="btn btn-info" onclick="showForm()">Add</button>
+    <div class="row">
+        <div id="add-button" class="col-md-12" align="center">
+            <button class="btn btn-info" onclick="showForm()">Add</button>
+        </div>
     </div>
-    <div id="form-container" class="col-md-6" style="display: none;">
-        <form method="post">
-            <h3 align="center">Submission Form</h3>
-            <br />
-            <?php echo $error; ?>
-            <div class="form-group">
-                <label>Enter Name</label>
-                <input type="text" name="name" placeholder="Enter Name" class="form-control" value="<?php echo $name; ?>" />
-            </div>
-            <div class="form-group">
-                <label>Enter Email</label>
-                <input type="text" name="email" class="form-control" placeholder="Enter Email" value="<?php echo $email; ?>" />
-            </div>
-            <div class="form-group">
-                <label>Enter Subject</label>
-                <input type="text" name="subject" class="form-control" placeholder="Enter Subject" value="<?php echo $subject; ?>" />
-            </div>
-            <div class="form-group">
-                <label>Enter Message</label>
-                <textarea name="message" class="form-control" placeholder="Enter Message"><?php echo $message; ?></textarea>
-            </div>
-            <div class="form-group" align="center">
-                <input type="submit" name="submit" class="btn btn-info" value="Submit" />
-            </div>
-        </form>
+    <div class="row">
+        <div class="col-md-12 table-container">
+            <h3 align="center">Expenses</h3>
+            <form method="post" onsubmit="return validateDelete()">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr style="font-weight: bold; color: white;">
+                        <th><input type="checkbox" onclick="toggleCheckbox(this)"></th>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Payed/Not Payed</th>
+                        <th>Amount ($)</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach($expenses_data as $key => $row): ?>
+                        <tr>
+                            <td><input type="checkbox" name="delete[]" value="<?php echo $key; ?>"></td>
+                            <td><?php echo $row[0]; ?></td>
+                            <td><?php echo $row[1]; ?></td>
+                            <td><?php echo $row[2]; ?></td>
+                            <td><?php echo $row[3]; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+                <div align="center">
+                    <button type="submit" name="delete-expense-submit" class="btn btn-danger">Delete Expense</button>
+                </div>
+                <?php echo $error_expense; ?>
+            </form>
+        </div>
+    </div>
+    <div class="row">
+        <div id="add-expense-button" class="col-md-12" align="center">
+            <button class="btn btn-info" onclick="showExpenseForm()">Add Expense</button>
+        </div>
+    </div>
+    <div class="row">
+        <div id="form-container" class="col-md-12" style="display: none;">
+            <form method="post">
+                <h3 align="center">Submission Form</h3>
+                <br />
+                <?php echo $error; ?>
+                <div class="form-group">
+                    <label>Enter Name</label>
+                    <input type="text" name="name" placeholder="Enter Name" class="form-control" value="<?php echo $name; ?>" />
+                </div>
+                <div class="form-group">
+                    <label>Enter Email</label>
+                    <input type="text" name="email" class="form-control" placeholder="Enter Email" value="<?php echo $email; ?>" />
+                </div>
+                <div class="form-group">
+                    <label>Enter Password</label>
+                    <input type="text" name="subject" class="form-control" placeholder="Enter Password" value="<?php echo $subject; ?>" />
+                </div>
+                <div class="form-group">
+                    <label>Enter Outstanding Balance</label>
+                    <textarea name="message" class="form-control" placeholder="Enter Outstanding Balance"><?php echo $message; ?></textarea>
+                </div>
+                <div class="form-group" align="center">
+                    <input type="submit" name="submit" class="btn btn-info" value="Submit" />
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="row">
+        <div id="form-expense-container" class="col-md-12" style="display: none;">
+            <form method="post">
+                <h3 align="center">Expense</h3>
+                <br />
+                <?php echo $error_expense; ?>
+                <div class="form-group">
+                    <label>Enter Name</label>
+                    <input type="text" name="name" placeholder="Enter Name" class="form-control" value="<?php echo $name; ?>" />
+                </div>
+                <div class="form-group">
+                    <label>Enter Category of Expense</label>
+                    <input type="text" name="category" class="form-control" placeholder="Enter Category" value="<?php echo $category; ?>" />
+                </div>
+                <div class="form-group">
+                    <label>Did you pay?</label>
+                    <input type="text" name="payed" class="form-control" placeholder="Enter Y or N" value="<?php echo $payed; ?>" />
+                </div>
+                <div class="form-group">
+                    <label>Enter amount you paid</label>
+                    <textarea name="balance" class="form-control" placeholder="Enter Amount"><?php echo $balance; ?></textarea>
+                </div>
+                <div class="form-group" align="center">
+                    <input type="submit" name="submit-expense" class="btn btn-info" value="Submit" />
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 </body>
 </html>
+
+
